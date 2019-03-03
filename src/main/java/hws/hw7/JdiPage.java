6a134d248ed9a4318778805291ad02f6095ce356
RinaVladimyrovna <efedorovaw@gmail.com>
2019-03-03 22:03:59 +0300

    hw7 uncomb, with titles

--- src/main/java/hws/hw7/JdiPage.java
+++ src/main/java/hws/hw7/JdiPage.java
@@ -1,6 +1,7 @@
 package hws.hw7;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
+import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 
 import hws.hw7.pages.JdiIndexPage;
@@ -10,9 +11,11 @@ import hws.hw7.pages.MetalAndColorPage;
 public class JdiPage {
 
     @Url("index.html")
+    @Title("Home Page")
     public static JdiIndexPage indexPageJdi;
 
     @Url("metals-colors.html")
+    @Title("Metal and Colors")
     public static MetalAndColorPage metalsColorsPage;
 
 }
